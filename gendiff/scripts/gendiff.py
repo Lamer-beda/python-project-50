e199e1eba5acfd95202a57abcc34b53dca8b8cb4
lamer <Pavellepel@yandex.ru>
2024-07-21 17:29:04 +0300

    2 ste[

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -6,10 +6,13 @@ def main():
     )
     parser.add_argument('first_file', type=str, help='First configuration file')
     parser.add_argument('second_file', type=str, help='Second configuration file')
+    parser.add_argument('-f', '--format', type=str, help='set format of output', default='stylish')
+
     args = parser.parse_args()
     # Here you would call the function that does the comparison and show the difference
     # For now, we just print the received arguments
     print(f"Comparing {args.first_file} and {args.second_file}")
+    print(f"Output format: {args.format}")
 
 if __name__ == '__main__':
     main()
