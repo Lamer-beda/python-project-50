f5bbb4faa102e6459106113a9aceee173d8a0708
lamer <Pavellepel@yandex.ru>
2024-07-28 21:24:50 +0300

    fix 8step

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,10 +1,37 @@
-from gendiff.parser import parse_file
-from gendiff.build_diff import make_diff
-from gendiff.formatter.format_router import formatter
+import json
+import yaml
 
+def generate_diff(file_path1, file_path2, format='stylish'):
+    # Load JSON or YAML files
+    with open(file_path1) as f1, open(file_path2) as f2:
+        if file_path1.endswith('.json'):
+            data1 = json.load(f1)
+            data2 = json.load(f2)
+        else:
+            data1 = yaml.safe_load(f1)
+            data2 = yaml.safe_load(f2)
 
-def generate_diff(path1, path2, output_format='stylish'):
-    data1 = parse_file(path1)
-    data2 = parse_file(path2)
-    diffs = make_diff(data1, data2)
-    return formatter(diffs, output_format).strip()
+    # Generate the diff
+    diff = {}
+    keys = set(data1.keys()).union(set(data2.keys()))
+    for key in sorted(keys):
+        if key in data1 and key in data2:
+            if data1[key] == data2[key]:
+                diff[key] = data1[key]
+            else:
+                diff[f"- {key}"] = data1[key]
+                diff[f"+ {key}"] = data2[key]
+        elif key in data1:
+            diff[f"- {key}"] = data1[key]
+        else:
+            diff[f"+ {key}"] = data2[key]
+
+    if format == 'stylish':
+        return json.dumps(diff, indent=4).replace('"', '').replace(',', '')
+    elif format == 'txt':
+        return '\n'.join(f'{k}: {v}' for k, v in diff.items())
+    elif format == 'yaml':
+        return yaml.dump(diff, default_flow_style=False)
+
+    return json.dumps(diff, indent=4)
+
