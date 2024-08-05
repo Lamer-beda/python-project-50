f58a39aab7478088a37a15d49d96a79d442e8ea3
lamer <Pavellepel@yandex.ru>
2024-08-05 20:20:32 +0300

    after first review

--- gendiff/generate_diff.py
+++ gendiff/generate_diff.py
@@ -1,5 +1,5 @@
 from gendiff.parser import parse_data_from_file
-from gendiff.generate import generate
+from gendiff.build_diff import generate
 from gendiff.formatters.choice_formatter import format_diff
 
 
@@ -7,4 +7,7 @@ def generate_diff(path_to_file1, path_to_file2, formatter='stylish'):
     content_file_1 = parse_data_from_file(path_to_file1)
     content_file_2 = parse_data_from_file(path_to_file2)
     diff = generate(content_file_1, content_file_2)
-    return format_diff(diff, formatter)
+    if formatter == 'stylish' or 'txt' or 'yaml' or 'json':
+        return format_diff(diff, formatter)
+    else:
+        raise NameError("Unsupported format")
